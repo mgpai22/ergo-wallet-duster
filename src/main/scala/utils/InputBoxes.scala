f1d6193ef437204fb7e3a9c12af9fd6c22c71eb7
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-07-28 01:30:09 -0400

    change getInputs method to take a Seq of tokens as input rather than ids

--- src/main/scala/utils/InputBoxes.scala
+++ src/main/scala/utils/InputBoxes.scala
@@ -15,31 +15,19 @@ class InputBoxes(val ctx: BlockchainContext) {
   def getInputs(
       amountList: Seq[Long],
       senderAddress: Address,
-      tokenListParam: Option[Seq[Seq[String]]] = None,
-      amountTokens: Option[Seq[Seq[Long]]] = None
+      tokens: Seq[ErgoToken] = Seq()
   ): Seq[InputBox] = {
     val amountTotal: Long = amountList.sum
-    val tokens = (tokenListParam, amountTokens) match {
-      case (Some(tlp), Some(at)) =>
-        tlp
-          .zip(at)
-          .flatMap { case (token, amounts) =>
-            token.zip(amounts).map { case (t, a) => new ErgoToken(t, a) }
-          }
-          .asJava
-      case (Some(tlp), None) =>
-        tlp.flatten.map(new ErgoToken(_, 1)).asJava
-      case _ =>
-        new util.ArrayList[ErgoToken]()
-    }
 
-    BoxOperations
+    val inputs = BoxOperations
       .createForSender(senderAddress, this.ctx)
       .withAmountToSpend(amountTotal)
-      .withTokensToSpend(tokens)
       .withInputBoxesLoader(new ExplorerAndPoolUnspentBoxesLoader())
-      .loadTop()
-      .asScala
+
+    if (tokens.nonEmpty) {
+      inputs.withTokensToSpend(tokens.asJava)
+    }
+    inputs.loadTop().asScala
   }
 
 }
